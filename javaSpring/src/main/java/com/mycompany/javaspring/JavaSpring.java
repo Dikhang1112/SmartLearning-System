efac85ca4f33e4a4ecaffa4c6c54d503cd9613d6
Dikhang1112 <duykhanggt5@gmail,com>
2025-07-15 22:11:59 +0700

    Begin

---
+++ javaSpring/src/main/java/com/mycompany/javaspring/JavaSpring.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaspring;
+
+/**
+ *
+ * @author AN515-57
+ */
+public class JavaSpring {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
